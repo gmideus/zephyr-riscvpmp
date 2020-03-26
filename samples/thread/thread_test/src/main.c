81e699675b560f5d367c678efea93c2f11e0e7b4
AntonioMorales97 <antonio_97@live.se>
2020-03-26 14:16:09 +0100

    each thread yields X times

--- samples/thread/thread_test/src/main.c
+++ samples/thread/thread_test/src/main.c
@@ -13,17 +13,21 @@
 #define NUM_THREADS 8
 #define THREAD_STACK_SIZE 500
 #define THREAD_PRIORITY 0 //default is 0 if !PREEMPT_ENABLED
+#define THREAD_YIELDS 10000
 
 struct k_thread worker_threads[NUM_THREADS];
 
 K_THREAD_STACK_ARRAY_DEFINE(worker_stacks, NUM_THREADS, THREAD_STACK_SIZE);
 
 int executed = 0;
+int global_yield_counter = 0;
 
 void threads_entry_point(void *p1, void *p2, void *p3){
-  executed++;
-  printk("Thread: %d \n", (int)POINTER_TO_INT(p1));
-  k_yield();
+  for(int i = 0; i < THREAD_YIELDS; i++){
+    global_yield_counter++;
+    k_yield();
+  }
+  //printk("Thread: %d \n", (int)POINTER_TO_INT(p1));
 }
 
 /**
@@ -36,6 +40,7 @@ void threads_entry_point(void *p1, void *p2, void *p3){
 void test_main(void)
 {
   u32_t start_time, stop_time, cycles_spent, nanoseconds_spent;
+  int count = 0;
   for (int i = 0; i < NUM_THREADS; i++) {
 		k_thread_create(&worker_threads[i],
 				worker_stacks[i], THREAD_STACK_SIZE,
@@ -43,13 +48,16 @@ void test_main(void)
 				THREAD_PRIORITY,
 				0, K_NO_WAIT);
 	}
-  zassert_true(executed == 0, "Threads ran too soon");
+  zassert_true(global_yield_counter == 0, "Threads ran too soon");
   start_time = k_cycle_get_32();
   k_yield();
-  /* We got rescheduled again */
+  while(global_yield_counter < NUM_THREADS * THREAD_YIELDS){
+    count++;
+    k_yield();
+  }
+  /* We got rescheduled again and we have exited while loop */
   stop_time = k_cycle_get_32();
-  zassert_true(executed == NUM_THREADS, "Not enough threads ran");
-
+  zassert_true(global_yield_counter == NUM_THREADS * THREAD_YIELDS, "Not enough threads ran");
   cycles_spent = stop_time - start_time;
   nanoseconds_spent = (u32_t)k_cyc_to_ns_floor64(cycles_spent);
   printk("Threads ran in: %u nanoseconds\n", nanoseconds_spent);
