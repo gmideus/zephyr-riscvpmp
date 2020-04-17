9c0fd60909d0b617871d95804031c0e1b78eef65
mideus <mideus@kth.se>
2020-04-17 16:22:51 +0200

    code area test

--- samples/code_area/code_area_test/src/main.c
+++ samples/code_area/code_area_test/src/main.c
@@ -9,38 +9,38 @@
 #include <stdlib.h>
 #include <sys/printk.h>
 #include <kernel.h>
-
-extern void test_write_to_code();
+volatile extern int test_func(int);
+extern void test_write_to_code(void*);
 
 int add_one(int a){
   return a + 1;
 }
 
-void write_to_code_area(){
-  test_write_to_code();
-  //int (*fun_ptr)(int) = (void*) &add_one;
-  int volatile * const fun_addr_ptr = (int *) &add_one;
-  *fun_addr_ptr = 0x00750513;
-  printk("%x\n", &fun_addr_ptr);
-  printk("%x\n", fun_addr_ptr);
-  //printk("%x\n", *fun_ptr);
-  printk("%x\n", *fun_addr_ptr);
-  int n = 1;
-  n = add_one(n);
-  printk("%d\n", n);
+//void write_to_code_area(){
 
-  n = 0x2;
-  printk("%d\n", n);
-  int * p = &n;
-  *p = 0x3;
-  printk("%d\n", n);
-}
+  //int (*fun_ptr)(int) = (void*) &add_one;
+  // int volatile * const fun_addr_ptr = (int *) &add_one;
+  // *fun_addr_ptr = 0x00750513;
+  // printk("%x\n", &fun_addr_ptr);
+  // printk("%x\n", fun_addr_ptr);
+  // //printk("%x\n", *fun_ptr);
+  // printk("%x\n", *fun_addr_ptr);
+  // int n = 1;
+  // n = add_one(n);
+  // printk("%d\n", n);
+  //
+  // n = 0x2;
+  // printk("%d\n", n);
+  // int * p = &n;
+  // *p = 0x3;
+  // printk("%d\n", n);
+//}
 
 
 void test_main(void)
 {
   int n = 1;
-  write_to_code_area();
-  n = add_one(n);
+  test_write_to_code(&test_func);
+  n = test_func(n);
   printk("%d\n", n);
 }
