d763fa89461741643b59da4e377628ae75af833a
AntonioMorales97 <antonio_97@live.se>
2020-05-11 14:02:05 +0200

    renamed from stack_test

---
+++ samples/riscv_user_mode/stack_execution_test/src/main.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <ztest.h>
+#include <stdlib.h>
+#include <sys/printk.h>
+#include <kernel.h>
+
+
+//extern void test_add();
+
+/**
+* CARE C OPTIMIZING
+*/
+void test() {
+
+  int inject [4] = {
+    0x00750513, 0x00008067, 0, 0
+  };
+
+  printk("Here we set fun ptr\n");
+  int (*fun_ptr)(int) = (void *) &inject[0];
+  printk("Here we go\n");
+  //test_add();
+  printk("%pn\n", &inject[0]);
+  printk("%d\n", inject[0]);
+  int a = (*fun_ptr)(1);
+  printk("%d \n", a);
+}
+
+void test_main(void)
+{
+  test();
+}
